668933c9da00cce271233354f9c10b96b600e2fd
anzanda <tjd1024@g.skku.edu>
2021-03-14 23:16:11 +0900

    7576완성

--- 7576.cpp
+++ 7576.cpp
@@ -1,104 +1,79 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
-int r, c, count;
+int r, c;
 int a[1000][1000];
-int sol[1000][1000];
 bool visited[1000][1000];
-void check(int x, int y){
-    if(x<0||x>=r||y<0||y>=c){
-        return;
-    }
-    if(visited[x][y]){
-        return;
-    }
-    if(a[x][y] == -1){
-        visited[x][y] = true;
-        return;
-    }
-    if(a[x][y] == 0){
-        visited[x][y] = true;
-        a[x][y] = 1;
-        return;
-    }
-    if(a[x][y] == 1){
-        visited[x][y] = true;
-        check(x+1,y);
-        check(x-1, y);
-        check(x, y+1);
-        check(x, y-1);
-        return;
-    }
-}
+int dx[4] = {1, 0 , -1, 0};
+int dy[4] = {0, 1, 0, -1};
+int main(void){
+    cin >> c >> r;
 
-bool solutionCheck(){
-    int i, j;
+    queue<pair<int, int>> q;
+    
+    int input_int;
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            cin >> input_int;
+            a[i][j] = input_int;
+            if(a[i][j] == 1){
+                visited[i][j] = true;
+                q.push(make_pair(i,j));
+            }
+            else if(a[i][j] == -1){
+                visited[i][j] = true;
+            }
+        }
+    }
+
+    if(q.size() == r*c){
+        cout << -1;
+        return 0;
+    }
+
+    int count = 0;
+    while(true){
+        queue<pair<int, int>> nextQ;
+        while(q.size()){
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            for(int i=0; i<4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx >=0 && nx<r && ny>=0 && ny<c){//0~r-1 , 0~c-1
+                    if(a[nx][ny] == 0){
+                        a[nx][ny] = 1;
+                        visited[nx][ny] = true;
+                        nextQ.push(make_pair(nx, ny));
+                    }
+                    else if(a[nx][ny] == -1){
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+        if(nextQ.empty()){
+            break;
+        }
+        q = nextQ;
+        count++;
+    }
     bool flag = false;
-    for(i=0; i<r; i++){
-        for(j=0; j<c; j++){
-            if(a[i][j] != sol[i][j]){
-                flag = true;
-                count++;
+    for(int i=0; i<r && !flag; i++){
+        for(int j=0; j<c; j++){
+            if(!visited[i][j]){
+                flag=true;
+                break;
             }
         }
     }
     if(!flag){
-        return true;
+        cout << count;
     }
     else{
-        return false;
+        cout << -1;
     }
-}
-int main(void){
-    cin >> c >> r;
-
-    int input_num;
-    int zerozCount = 0;
-    for(int i=0; i<r; i++){
-        for(int j=0; j<c; j++){
-            cin >> input_num;
-            a[i][j] = input_num;
-            if(!input_num){
-                sol[i][j] = 1;
-                zeroCount++;
-            }    
-            else
-                sol[i][j] = input_num;
-        }
-    }
-    if(zeroCount == 0){
-        cout << 0 << '\n';
-        return 0;
-    }
-
-    int num=1;
-    while(true){
-        for(int i=0; i<r; i++){//1È®Àå!
-            for(int j=0; j<c; j++){
-                if(a[i][j])
-                    check(i,j);
-            }
-        }
-
-        int tmp = count;
-        count = 0;
-        if(solutionCheck()){
-            cout << num << '\n';
-            break;
-        }
-        if(tmp == count)
-        {
-            cout << -1 << '\n';
-            break;
-        }
-
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
-                visited[i][j] = false;
-            }
-        }
-
-        num++;
-    }    
     return 0;
 }
