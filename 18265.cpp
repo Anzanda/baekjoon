7f2c14e72ffc44803ffe05462b7d5701279d4d47
anzanda <tjd1024@g.skku.edu>
2021-04-06 13:37:22 +0900

    이윤성식 바이너리서치

--- 18265.cpp
+++ 18265.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
 int main(void){
@@ -7,17 +7,23 @@ int main(void){
     ll start =0;
     ll end = INT32_MAX;
     ll ans =0;
-    while(start<=end){
+    while(1){
         ll mid = (start+end) >> 1;
         ans = mid - (mid/3) - (mid/5) + (mid/15);
        // printf("ans: %d\n", ans);
         if(ans < n){
             start = mid+1;
         }
-        else{
+        else if(ans > n){
             end = mid-1;
         }
+        else{
+           // printf("before mid: %d\n", mid);
+            while(mid%3==0 || mid%5==0) mid--;
+            printf("%d", mid);
+            break;
+        }
+        
     }
-    cout << start;
     return 0;
 }
