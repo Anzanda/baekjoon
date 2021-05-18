41d158cb0e31b974491a1c9b13be807f366616c9
anzanda <tjd1024@g.skku.edu>
2021-05-18 17:03:06 +0900

    1199 배열로 푸니까 풀림 씹

--- 1199.cpp
+++ 1199.cpp
@@ -3,54 +3,38 @@ using namespace std;
 #define MAXN 1000
 
 int n;
-vector<int> adj[MAXN+1];
+int adj[MAXN+1][MAXN+1];
+int degree[MAXN+1];
 vector<int> ans;
-bool visited[MAXN+1][MAXN+1];
-void printAll();
 void dfs(int x){
-    for(auto there: adj[x]){
-        if(!visited[x][there]){
-            visited[x][there]=true;
-            visited[there][x]=true;
-            dfs(there);
+    for(int i=1; i<=n; i++){
+        while(adj[x][i]){
+            adj[x][i]--;
+            adj[i][x]--;
+            dfs(i);
         }
     }
     ans.push_back(x);
 }
-int degree[MAXN+1];
 int main(void){
     cin >> n;
     for(int i=1; i<=n; i++){
         for(int j=1; j<=n; j++){
-            int num;
-            cin >> num;
-            degree[i] += num;
-            degree[j] += num;
-            if(num){
-                adj[i].push_back(j);
-            }
+            cin >> adj[i][j];
+            degree[i] += adj[i][j];
+            degree[j] += adj[i][j];           
         }
     }
     for(int i=1; i<=n; i++){
         degree[i] /= 2;
         if(degree[i] % 2){
-            cout << -1 << '\n';
+            cout << -1 << "\n";
             return 0;
         }
     }
     dfs(1);
-    for(auto it=ans.rbegin(); it!=ans.rend(); it++){
+    for(auto it = ans.rbegin(); it!=ans.rend(); it++){
         cout << *it << " ";
     }
     return 0;
-}
-
-void printAll(){
-    for(int i=1; i<=n; i++){
-        printf("adj[%d] -> ", i);
-        for(auto t: adj[i]){
-            printf("%d ", t);
-        }
-        printf("\n");
-    }
 }
