92f21dfb719ef61921290710c89f6ad3197d3b72
anzanda <tjd1024@g.skku.edu>
2021-05-20 09:45:19 +0900

    15650

---
+++ 15650.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+void foo(int* arr, int x, int cnt){
+    
+    arr[cnt]=x;
+    
+    if(cnt == m-1){
+        for(int i=0; i<m; i++){
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        free(arr);
+        return;
+    }
+    for(int i=x+1; i<=n; i++){
+        int* v = (int*)malloc(sizeof(int)*m);
+        memcpy(v, arr, sizeof(int)*m);
+        foo(v, i, cnt+1);
+    }
+    free(arr);
+}
+int main(void){
+    cin >> n >> m;
+    int* arr;
+    for(int i=1; i<=n; i++){
+        arr = (int*)malloc(sizeof(int)*m);
+        foo(arr, i, 0);
+    }
+    
+    return 0;
+}
