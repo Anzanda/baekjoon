55bb70be25bb2ff222937c4f60f4ac24af826b94
anzanda <tjd1024@g.skku.edu>
2021-05-20 09:51:12 +0900

    15650-1 했슴  메모리절약풀이

--- 15650-1.cpp
+++ 15650-1.cpp
@@ -1,33 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int n, m;
-void foo(int* arr, int x, int cnt){
-    
-    arr[cnt]=x;
-    
+#define MAXN 8
+int n,m;
+int arr[MAXN];
+void foo(int x, int cnt){
     if(cnt == m-1){
         for(int i=0; i<m; i++){
             cout << arr[i] << " ";
         }
         cout << "\n";
-        free(arr);
         return;
     }
+    arr[cnt] = x;
     for(int i=x+1; i<=n; i++){
-        int* v = (int*)malloc(sizeof(int)*m);
-        memcpy(v, arr, sizeof(int)*m);
-        foo(v, i, cnt+1);
+        foo(i, cnt+1);
+        arr[cnt] = x;
     }
-    free(arr);
 }
 int main(void){ios_base::sync_with_stdio(false);cin.tie(nullptr);
     cin >> n >> m;
-    int* arr;
     for(int i=1; i<=n; i++){
-        arr = (int*)malloc(sizeof(int)*m);
-        foo(arr, i, 0);
+        foo(i,0);
     }
-    
-    return 0;
 }
