08f9c10a675e53788f0ca7a6df5b7d12af627aa1
anzanda <tjd1024@g.skku.edu>
2021-06-24 01:34:38 +0900

    dfs 권위자 바로 나

--- 2098.cpp
+++ 2098.cpp
@@ -1,61 +1,43 @@
 #include <bits/stdc++.h>
 #define MAXN 16
+#define INF 987654321
 using namespace std;
 
-int n;
+int n, all;
 int w[MAXN][MAXN];
-int dp[MAXN][(1<<MAXN)];
-void Print(int bit){
-    printf(" bit: ");
-    int mask;
-    for(int i=n-1; i>=0; i--){
-        mask = bit&(1<<i);
-        cout << (mask>0);
+int dp[MAXN][1<<MAXN];
+void dfs(int now, int bit) {
+    if(bit == all) {
+        if(w[now][0]) {
+            dp[now][bit] = w[now][0];
+        }
+        else {
+            dp[now][bit] = INF;
+        }  
     }
-    cout << endl;
-}
-// void dfs(int now, int bit){
-//     // cout << "now: " << now;
-//     // Print(bit);
-//     if(bit&(1)){
-//         return;
-//     }
-//     for(int i=0; i<n; i++){
-//         int mask = bit&(1<<i);
-//         if(w[now][i] && mask == 0){
-//             int tmp = dp[bit] + w[now][i];
-//             if(tmp < dp[bit+(1<<i)]){
-//                 dp[bit+(1<<i)] = tmp;
-//                 dfs(i, bit+(1<<i));
-//             }
-//         }
-//     }
-// }
-int dfs(int now, int bit){
-    if(bit == (1<<n)-1){
-        if(!w[now][0])  return INT_MAX;
-        else    return w[now][0];
+    if(dp[now][bit] != 0) {//0이 아니라는 것은 이미 해당노드를 기준으로 탐색을 했다는 의미.
+    //탐색을 했다면 무조건 최소인 값이 담겨있을 것이므로 return;을 해준다.
+        return;
     }
-    if(dp[now][bit] != -1){
-        return dp[now][bit];
-    }
-    dp[now][bit] = INT_MAX;
-    for(int i=0; i<n; i++){
-        if(w[now][i] == 0)  continue;
-        if((bit&(1<<i)) == (1<<i))    continue;
 
-        dp[now][bit] = min(dp[now][bit], w[now][i]+dfs(i,bit|(1<<i)));
+    dp[now][bit] = INF;
+    for(int i=0; i<n; i++){
+        if((bit&(1<<i)) != (1<<i) && w[now][i]){
+            dfs(i, bit|1<<i);
+            dp[now][bit] = min(dp[now][bit], w[now][i]+dp[i][bit|1<<i]);
+        }
     }
-    return dp[now][bit];
 }
 int main(void){
     cin >> n;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
+    all = (1<<n)-1;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
             cin >> w[i][j];
         }
     }
-    memset(dp, -1, sizeof(dp));
-    cout << dfs(0, 1) << "\n";
+    dfs(0, 1);
+    cout << dp[0][1] << "\n";
+    
     return 0;
 }
