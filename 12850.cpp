52438f3b4b216a6474921ebd11bef473805d99bb
anzanda <tjd1024@g.skku.edu>
2021-07-30 01:12:41 +0900

    12850 - 본대산책2

--- 12850.cpp
+++ 12850.cpp
@@ -1,66 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define pb push_back
-int D;
-int dp[(1<<14)];
-bool vst[(1<<14)];
-bool node[8];
-vector<int> g[8];
-void init() {
-    g[0].pb(1), g[0].pb(2);
-    g[1].pb(0), g[1].pb(2), g[1].pb(3);
-    g[2].pb(0), g[2].pb(1), g[2].pb(3), g[2].pb(4);
-    g[3].pb(1), g[3].pb(2), g[3].pb(4), g[3].pb(5);
-    g[4].pb(2), g[4].pb(3), g[4].pb(5), g[4].pb(6);
-    g[5].pb(3), g[5].pb(4), g[5].pb(7);
-    g[6].pb(4), g[6].pb(7);
-    g[7].pb(5), g[7].pb(6);
-}
-void printBit(int mask) {
-    cout << "mask: " << mask << endl;
-    for(int i=0; i<14; i++) {
-        if(mask&(1<<i)) {
-            cout << 1;
-        } else {
-            cout << 0;
-        }
+#define endl "\n"
+#define ll long long
+#define FastIO ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define MOD 1000000007LL
+
+vector<vector<ll>> v = {
+    {0, 1, 1, 0, 0, 0, 0, 0},
+    {1, 0, 1, 1, 0, 0, 0, 0},
+    {1, 1, 0, 1, 1, 0, 0, 0},
+    {0, 1, 1, 0, 1, 1, 0, 0},
+    {0, 0, 1, 1, 0, 1, 0, 1},
+    {0, 0, 0, 1, 1, 0, 1, 0},
+    {0, 0, 0, 0, 0, 1, 0, 1},
+    {0, 0, 0, 0, 1, 0, 1, 0}
+};
+vector<vector<ll>> mul(vector<vector<ll>>& m1, vector<vector<ll>>& m2) {
+    vector<vector<ll>> ret(8, vector<ll>(8));
+    for(int i=0; i<8; i++) {
+        for(int j=0; j<8; j++) {
+            ll tmp = 0;
+            for(int k=0; k<8; k++) {
+                tmp = (tmp + (m1[i][k] * m2[k][j]))%MOD;
+            }
+            ret[i][j] = tmp%MOD;
+        }    
     }
-    cout << endl;
+    return ret;
 }
-void dfs(int now, int edge, int mask) {
-    printBit(mask);
-    vst[mask] = true;
-    for(auto there: g[now]) {
-        int nextEdge= now + there;
-        cout << "nextEdge: " << nextEdge << endl;
-        if(!(mask&(1<<nextEdge))) {
-            dfs(there, nextEdge, mask|(1<<nextEdge));        
-        }
-    }
-}
+ll D;
 int main(void) {
     cin >> D;
-    init();
-    dfs(0, 0, 0);
-    int ans = 0;
-    cout << endl;
-    for(int i=0; i<(1<<14); i++) {
-        if(vst[i] == true) {
-            int cnt = 0;
-            for(int j=0; j<14; j++) {
-                if((i&(1<<j))) {
-                    cnt++;
-                }
-            }            
-            if(D > cnt*2) {
-                int tmp = D - cnt * 2;
-                if(tmp%2 == 0) {
-
-                } else {
-                    continue;
-                }
-            } 
-        }
+    vector<vector<ll>> ans(8, vector<ll>(8));
+    for(int i=0; i<8; i++){ 
+        ans[i][i] = 1;
     }
+    vector<vector<ll>> factor = v;
+    while(D) {
+        if(D & 1) {
+            ans = mul(ans, factor);
+            D -= 1;
+        }
+        factor = mul(factor, factor);
+        D /= 2;
+    }
+    cout << ans[0][0] << endl;
+    return 0;
 }
