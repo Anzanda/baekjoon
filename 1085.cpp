c48cba248846518a6621cee41dd0dd3173e705bd
ipad <tjd1024@g.skku.edu>
2021-08-01 15:13:21 +0000

    1085번 - 직사각형에서 탈출

--- 1085.cpp
+++ 1085.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int x, y, w, h;
 int main (){
 	cin >> x >> y >> w >> h;
-	int ans = min(abs(x-w), abs(y-h));
-	ans = min(abs(x), abs(y));
+	x = min(x, abs(w-x));
+	y = min(y, abs(h-y));
+	ans = min(x, y);
 	cout << ans << endl;
 	return 0;
 }
