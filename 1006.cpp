6ea7e973e21cb26217f9c8977495c237ba628ead
anzanda <tjd1024@g.skku.edu>
2021-08-02 17:12:58 +0900

    1006 - 습격자 초라기(anzanda's 코드)

--- 1006.cpp
+++ 1006.cpp
@@ -2,72 +2,76 @@
 using namespace std;
 
 #define endl "\n"
+#define FastIO ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
 #define MAX 10000
-#define FastIO ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
-#define INF 987645321
+#define INF 987654321
 
-int T, n, w;   
-vector<vector<int>> v;
-vector<vector<int>> dp;
+int t, n, w;
+int enemy[MAX][2];
+int dp[MAX][3];
+/*
+    j=0일 때, i번째가 위 아래 둘 다 채워진 경우중 최솟값
+    j=1일 때, i번째가 위만 채워진 경우중 최솟값
+    j=2일 때, i번째가 아래만 채워진 경우중 최솟값
+*/
+int ans;
 void rundp() {
-    for(int i=2; i<=n; i++) {
-        int up = v[0][i-1] + v[0][i] <= w ? 1 : 2;
-        int down = v[1][i-1] + v[1][i] <= w ? 1 : 2;
-        int verticle = v[0][i] + v[1][i] <= w ? 1: 2;
-
-        dp[i][0] = min({dp[i-1][0]+verticle, dp[i-2][0]+up+down, dp[i-1][1]+up+1, dp[i-1][2]+down+1});
-        dp[i][1] = min(dp[i-1][2]+down, dp[i-1][0]+1);
-        dp[i][2] = min(dp[i-1][1]+up, dp[i-1][0]+1);
+    int section1 = enemy[0][0] + enemy[1][0] <= w ? 1:2;
+    int section2 = enemy[0][1] + enemy[1][1] <= w ? 1:2;
+    int verticle = enemy[1][0] + enemy[1][1] <= w ? 1:2;
+    dp[1][0] = min({section1+section2, dp[0][0]+verticle, dp[0][1]+section2+1, dp[0][2]+section1+1});
+    dp[1][1] = min(dp[0][0]+1, dp[0][2]+section1);
+    dp[1][2] = min(dp[0][0]+1, dp[0][1]+section2);
+    for(int i=2; i<n; i++) {
+        section1 = enemy[i-1][0] + enemy[i][0] <= w ? 1:2;
+        section2 = enemy[i-1][1] + enemy[i][1] <= w ? 1:2;
+        verticle = enemy[i][0] + enemy[i][1] <= w ? 1:2;
+        dp[i][0] = min({dp[i-2][0]+section1+section2, dp[i-1][0]+verticle, dp[i-1][1]+section2+1, dp[i-1][2]+section1+1});
+        dp[i][1] = min(dp[i-1][0]+1, dp[i-1][2]+section1);
+        dp[i][2] = min(dp[i-1][0]+1, dp[i-1][1]+section2);
     }
+    return;
 }
-int main(void) {  
+int main(void) {
     FastIO;
-    cin >> T;
-    while(T--) {
+    cin >> t;
+    while(t--) {
         cin >> n >> w;
-        v = vector<vector<int>>(2, vector<int>(n+1, 0));
-        dp = vector<vector<int>>(n+1, vector<int>(3, 0));
-        for(int i=0; i<2; i++) {
-            for(int j=1; j<=n; j++) {
-                cin >> v[i][j];
-            }
-        } 
-        vector<int> tmp = {v[0][1], v[1][1]};
-
-        int ans=INF;
-        dp[1][0] = v[0][1] + v[1][1] <= w ? 1:2;
-        dp[1][1] = dp[1][2] = 1;
+        for(int i=0; i<n; i++)  cin >> enemy[i][0];
+        for(int i=0; i<n; i++)  cin >> enemy[i][1];
+        vector<int> section = {enemy[0][0], enemy[0][1]};
+        dp[0][0] = enemy[0][0] + enemy[0][1] <= w ? 1:2;
+        dp[0][1] = dp[0][2] = 1;
         rundp();
-        ans = min(ans, dp[n][0]);
-        if(v[0][1] + v[0][n] <= w) {
-            dp[1][0] = 2;
-            dp[1][1] = dp[1][2] = 1;
-            v[0][1] = INF;
+        ans = dp[n-1][0];
+        if(enemy[0][0]+enemy[n-1][0] <= w) {
+            enemy[0][0] = INF;
+            dp[0][0] = 2;
+            dp[0][1] = dp[0][2] = 1;
             rundp();
-            ans = min(ans, dp[n][1]);
-            v[0][1] = tmp[0]; 
+            ans = min(ans, dp[n-1][2]);
+            enemy[0][0] = section[0];
         }
-        if(v[1][1] + v[1][n] <= w) {
-            dp[1][0] = 2;
-            dp[1][1] = dp[1][2] = 1;
-            v[1][1] = INF;
+        if(enemy[0][1]+enemy[n-1][1] <= w) {
+            enemy[0][1] = INF;
+            dp[0][0] = 2;
+            dp[0][1] = dp[0][2] = 1;
             rundp();
-            ans = min(ans, dp[n][2]);
-            v[1][1] = tmp[1]; 
+            ans = min(ans, dp[n-1][1]); 
+            enemy[0][1] = section[1];
         }
-        if(v[0][1]+v[0][n] <= w && v[1][1]+v[1][n] <= w) {
-            v[0][1] = v[1][1] = INF;
-            dp[1][0] = 2;
-            dp[1][1] = dp[1][2] = 1;
+        if(enemy[0][0]+enemy[n-1][0]<=w && enemy[0][1]+enemy[n-1][1]<=w) {
+            enemy[0][0] = enemy[0][1] = INF;
+            dp[0][0] = 2;
+            dp[0][1] = dp[0][2] = 1;
             rundp();
-            ans = min(ans, dp[n-1][0]);
-            v[0][1] = tmp[0];
-            v[1][1] = tmp[1];
+            ans = min(ans, dp[n-2][0]);
+            enemy[0][0] = section[0];
+            enemy[0][1] = section[1];
         }
         if(n == 1) {
-            ans = v[0][1] + v[1][1] <= w ? 1:2;
+            ans = enemy[0][1] + enemy[0][0] <= w ? 1:2;
         }
         cout << ans << endl;
     }
-    return 0;
 }
