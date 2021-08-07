51df8a9829f6735ea3363123e3341dfb1a8b9288
ipad <tjd1024@g.skku.edu>
2021-08-07 17:31:24 +0000

    fix 19539.cpp better code

--- 19539.cpp
+++ 19539.cpp
@@ -10,32 +10,17 @@ int main(void) {
 	FastIO;
 	cin >> n;
 	int num;
-	int one, modOne, modTwo;
-	one = modOne = modTwo = 0;
+	int modOne, modTwo;
+	modOne = modTwo = 0;
 	for(int i=0; i<n; i++) {
 		cin >> num;
-		if(num == 1) {
-			one++;
-		} else if(num%3 == 1) {
+		if(num%3 == 1) {
 			modOne++;
 		} else if(num%3 == 2) {
 			modTwo++;
 		}
 	}
-	bool ans = false;
-	if(one <= modTwo) {
-		modTwo -= one;
-		if(modTwo > modOne) {
-			modTwo -= modOne;
-			modOne = 0;
-		} else {
-			modOne -= modTwo;
-			modTwo = 0;
-		}
-		if(modOne%3 == 0 && modTwo%3 == 0) {
-			ans = true;
-		}
-	}
+	bool ans = modOne%3 == modTwo%3;
 	if(ans) {
 		cout << "YES" << endl;
 	} else {
