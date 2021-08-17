aa48802f0637ac9ef0725b45dcc5833c99136611
anzanda <tjd1024@g.skku.edu>
2021-08-17 05:43:15 +0000

    18222 first submit!

---
+++ 18222.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define FastIO ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+
+using ll=long long;
+
+ll k;
+int finalBit[32];
+int main(void) {
+	cin >> k;
+	for(int i=0; i<32; i++) {
+		finalBit[i] = i%2;
+	}
+	int ans;
+	bool reverseFlag = false;
+	for(int i=31; i>=0; i--) {
+		if(k&(1<<i)) {
+			if(reverseFlag) {
+				ans = finalBit[i]? 0:1;
+				reverseFlag = false;
+			} else {
+				ans = finalBit[i];
+				reverseFlag = true;
+			}
+		}
+	}
+	cout << ans << endl;
+	return 0;
+}
