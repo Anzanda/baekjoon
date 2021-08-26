d68428d7255681b28a7f1bbe1ba36381f5bb0a72
anzanda <tjd1024@g.skku.edu>
2021-08-26 17:18:37 +0000

    1978 first submit

---
+++ 1978.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define FastIO cin.tie(0)->sync_with_stdio(0)
+
+const int MAX = 1024;
+
+int n;
+bool prime[MAX];
+int arr[100];
+int ans;
+void input() {
+	cin >> n;
+	for(int i=0; i<n; i++) {
+		cin >> arr[i];
+	}
+}
+void initPrime() {
+	memset(prime, true, sizeof prime);
+	prime[0] = prime[1] = false;
+	for(int i=2; i<sqrt(MAX); i++) {
+		for(int j=i*i; j<=MAX; j+=i) {
+			if(!prime[j]) continue;
+			prime[j] = false;
+		}
+	}
+}
+void solve() {
+	initPrime();
+	for(int i=0; i<n; i++) {
+		ans += prime[arr[i]];
+	}
+	cout << ans << endl;
+}
+int main(void) {
+	FastIO;
+	input();
+	solve();
+	return 0;
+}
