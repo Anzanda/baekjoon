16b5b5ff3f900ae9752da21d1e5586ec9849b917
anzanda <tjd1024@g.skku.edu>
2021-09-01 23:47:07 +0900

    3015 third submit

--- 3015.cpp
+++ 3015.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 #define FastIO cin.tie(0)->sync_with_stdio()
 #define endl "\n"
+#define ff first
+#define ss second
 
 using ll = long long;
+using pii = pair<int,int>;
 
 const int MAX = 5*1e5;
 
@@ -17,28 +20,34 @@ void input() {
 	}
 }
 void solve() {
-	ll ans = 0;
-	stack<int> s;
-	for(int i=0; i<n; i++) {
-		if(s.empty()) {
-			s.push(arr[i]);
-			continue;
-		}
-		if(s.top() < arr[i]) {
-			ans += (ll)s.size();
-			while(s.top() < arr[i]) {
-				s.pop();
-				if(s.empty()) {
-					break;
-				}
-			}
-			s.push(arr[i]);
-		} else {
-			ans += (ll)s.size();
-			s.push(arr[i]);
-		}
-	}
-	cout << ans << endl;
+    ll ans = 0;
+    stack<pii> s;
+    for(int i=0; i<n; i++) {
+        if(s.empty()) {
+            s.push({arr[i], 1});
+            continue;
+        }
+        if(s.top().ff < arr[i]) {
+            while(s.top().ff < arr[i]) {
+                ans += s.top().ss;
+                s.pop();
+                if(s.empty()) { // 나만의 가독성을 위해ㅋㅋ;
+                    break;
+                }
+            }
+            i--; // top() <= arr[i]상태로 만들고 i로 다시 for문기돌리기
+        } else if(s.top().ff == arr[i]){
+            ans += s.top().ss;
+            s.top().ss++;
+            if(s.size() > 1) {
+                ans++;
+            }
+        } else {
+            s.push({arr[i], 1});
+            ans++;
+        }
+    }
+    cout << ans << endl;
 }
 int main(void) {
 	FastIO;
