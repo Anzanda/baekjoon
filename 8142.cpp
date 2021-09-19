b91c8c2e43362f02ec700581762da90f8c7f9690
anzanda <tjd1024@g.skku.edu>
2021-09-19 18:08:52 +0000

    8142 third submit
    - BFS로풀이 선회

--- 8142.cpp
+++ 8142.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 #define FastIO cin.tie(0)->sync_with_stdio(0)
 #define endl "\n"
+#define ff first
+#define ss second
+
+using pi = pair<int,int>;
 
 const int MAX = 1000;
 
@@ -25,72 +29,46 @@ bool isRange(int r, int c) {
 	}
 	return true;
 }
-const int FAIL = -1;
-const int NORMAL = 0;
-const int VALLEY = 1;
-const int RIDGE = 2;
-int dfs(int r, int c) {
-	vst[r][c] = true;
-	int ret = NORMAL;
-	for(int i=0; i<8; i++) {
-		if(ret == FAIL) {
-			return ret;
-		}
-		int nr = r + dr[i];
-		int nc = c + dc[i];
-		if(isRange(nr, nc)) {
-			if(arr[r][c] == arr[nr][nc]) {
-				if(vst[nr][nc])	continue;
-				if(ret == NORMAL) {
-					ret = dfs(nr, nc);
-				} else {
-					int val = dfs(nr, nc);
-					if(val != NORMAL)	ret = val != ret? FAIL:ret;
-				}
-			} else {
-				int val = arr[nr][nc]-arr[r][c] > 0 ? VALLEY:RIDGE;
-				if(ret == NORMAL) {
-					ret = val;
-				} else {
-					ret = val != ret? FAIL:ret;
-				}
-			}
-		}
-	}
-	return ret;
-}
-bool allTheSame() {
-	int val = arr[0][0];
-	for(int i=0; i<n; i++) {
-		for(int j=0; j<n; j++) {
-			if(arr[i][j] != val) {
-				return false;
-			}
-		}
-	}
-	return true;
-}
-void solve() {
+pi bfs(int r, int c) {
 	int valley = 0;
 	int ridge = 0;
-	if(allTheSame()) {
-		cout << 1 << " " << 1 << endl;
-		return;
+	vst[r][c] = true;
+	queue<pi> q;
+	q.push({r,c});
+	while(!q.empty()) {
+		pi t = q.front();	q.pop();
+		for(int i=0; i<8; i++) {
+			int nr = t.ff + dr[i];
+			int nc = t.ss + dc[i];
+			if(!isRange(nr, nc))	continue;
+			if(!vst[nr][nc] && arr[t.ff][t.ss] == arr[nr][nc]) {
+				vst[nr][nc] = true;
+				q.push({nr, nc});
+			} else {
+				valley += arr[t.ff][t.ss]<arr[nr][nc];	
+				ridge += arr[t.ff][t.ss]>arr[nr][nc];
+			}
+		}
 	}
+	int retRidge = !valley&&ridge;
+	int retValley = !ridge&&valley; 	
+	return {retRidge, retValley};
+}
+void solve() {
+	pi ans = {0 ,0};
 	for(int i=0; i<n; i++) {
 		for(int j=0; j<n; j++) {
 			if(!vst[i][j]) {
-				int val = dfs(i, j);
-				if(val == VALLEY) {
-					valley++;
-				}
-				if(val == RIDGE) {
-					ridge++;
-				}
+				pi res = bfs(i, j);
+				ans.ff += res.ff;
+				ans.ss += res.ss;
 			}
 		}
 	}
-	cout << ridge << " " << valley << endl;
+	if(!ans.ff&&!ans.ss) {
+		ans.ff = ans.ss = 1;
+	}
+	cout << ans.ff << " " << ans.ss << endl;
 }
 int main(void) {
 	FastIO;
