e6bc2d16a492e19deaec9f2ebcc3c90731b5e168
anzanda <tjd1024@g.skku.edu>
2021-09-21 12:51:59 +0000

    8198 first submit

---
+++ 8198.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO cin.tie(0)->sync_with_stdio(0)
+#define endl "\n"
+
+using ll = long long;
+
+const int MX = 1e6;
+
+int n, k;
+ll m;
+int a[MX];
+int jmp[MX];
+void input() {
+	cin >> n >> k;
+	cin >> m;
+	for(int i=0; i<n; i++) {
+		cin >> a[i];
+	}
+}
+int getDist(int a, int b) {
+	return abs(a-b);
+}
+void getJmp() {
+	int l = 0;
+	int r = k;
+	for(int i=0; i<n; i++) {
+		while(r+1<n && getDist(a[i], a[l])>getDist(a[i], a[r+1])) {
+			l++;
+			r++;
+		}
+		if(getDist(a[i],a[l]) < getDist(a[i], a[r])) {
+			jmp[i] = r;
+		} else {
+			jmp[i] = l;
+		}
+	}
+}
+int ans[MX];
+int tmp[MX];
+void solve() {
+	while(m) {
+		if(m&1) {
+			for(int i=0; i<n; i++) {
+				ans[i] = jmp[i];
+			}
+		}
+		for(int i=0; i<n; i++) {
+			tmp[i] = jmp[jmp[i]];
+		}
+		for(int i=0; i<n; i++) {
+			jmp[i] = tmp[i];
+		}
+		m >>= 1;
+	}
+}
+void printAnswer() {
+	for(int i=0; i<n; i++)	cout << ans[i] +  1 << " ";
+	cout << endl;
+}
+int main(void) {
+	FastIO;
+	input();
+	getJmp();
+	solve();
+	printAnswer();
+	return 0;
+}
